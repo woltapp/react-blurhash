b46e98fb8f7a2ec8fd0f9b7033077005b707935e
Mathis Møller <mathis.moller@wolt.com>
2023-01-09 09:23:09 +0100

    Fix build output

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,20 +4,7 @@ export default defineConfig([
   {
     name: 'main',
     entry: ['./src/index.ts'],
-    outDir: './lib',
-    format: ['cjs'],
-    legacyOutput: true,
-    sourcemap: true,
-    clean: true,
-    splitting: false,
-    dts: false,
-    minify: true,
-  },
-  {
-    name: 'esm',
-    entry: ['./src/index.ts'],
-    outDir: './es',
-    format: ['esm'],
+    format: ['cjs', 'esm'],
     legacyOutput: true,
     sourcemap: true,
     clean: true,
@@ -28,7 +15,6 @@ export default defineConfig([
   {
     name: 'typedefs',
     entry: ['./src/index.ts'],
-    outDir: './lib',
     clean: false,
     dts: {
       only: true,
